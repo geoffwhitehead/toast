159ca0f30470a835a974e80cb37e53332f17d539
Gavin Gregory <gavin.ian.gregory@gmail.com>
2016-09-24 13:57:49 +0100

    Added CORS support to express server.

--- server/index.js
+++ server/index.js
@@ -36,6 +36,15 @@ io.on('connection', function (socket) {
     });
 });
 
+/**
+ * CORS
+ */
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /**
  * Routes
